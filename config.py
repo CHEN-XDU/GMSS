51e133a8db4224f146ae8ebbce8d0bc4df8ffb60
CHEN-XDU <102231826+CHEN-XDU@users.noreply.github.com>
2022-03-23 20:58:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import torch
 
 epochs = 100
-batch_size = 256 # 200 400 worse around 33-35 （ what is funny that the loss of them lower than 300, around 1.65)
+batch_size = 256 
 lr = 0.01
 weight_decay = 8e-5
 drop_rate = 0.25 
