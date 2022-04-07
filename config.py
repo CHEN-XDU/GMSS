56c7ec2cd40925f2c7aa7dac745fd310d7af0e01
CHEN-XDU <102231826+CHEN-XDU@users.noreply.github.com>
2022-04-07 17:49:46 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import torch
 
+
 epochs = 100
 batch_size = 256 
 lr = 0.01
 weight_decay = 8e-5
 drop_rate = 0.25 
 num_workers = 8
-device = ('cuda:0' if torch.cuda.is_available() else 'cpu')
+device = ('cuda:0' if torch.cuda.is_available() else 'cpu' )
 num_jigsaw = 4
 K = 2
